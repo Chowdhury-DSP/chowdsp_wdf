2a57929fcd514178c5aad3a5fc19542d674ef901
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-05-04 15:35:53 +0100

    Try to fix MSVC issue

--- include/chowdsp_wdf/wdft/wdft_sources.h
+++ include/chowdsp_wdf/wdft/wdft_sources.h
@@ -97,7 +97,7 @@ namespace wdft
         T R_value = (T) 1.0e-9;
     };
 
-    /** WDF Current source (non-adpatable) */
+    /** WDF Current source (non-adaptable) */
     template <typename T, typename Next>
     class IdealCurrentSourceT final : public RootWDF
     {
