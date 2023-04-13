0bf2803ae70f9b0ce59db2a69227f65e2614ed8f
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2023-04-13 02:52:11 -0700

    Add Resistor/Capacitor combo elements (#20)
    * Add Resistor/Capacitor combo elements
    
    * Update docs and add polymorphic wrappers

--- include/chowdsp_wdf/wdft/wdft_one_ports.h
+++ include/chowdsp_wdf/wdft/wdft_one_ports.h
@@ -385,6 +385,183 @@ namespace wdft
         T b_coef;
         T a_coef;
     };
+
+    /** WDF Resistor and Capacitor in Series */
+    template <typename T>
+    class ResistorCapacitorSeriesT final : public BaseWDF
+    {
+    public:
+        /** Creates a new WDF Resistor/Capacitor Series.
+         * @param cap_value: Resistance value in Ohms
+         * @param res_value: Capacitance value in Farads
+         * @param fs: WDF sample rate
+         */
+        explicit ResistorCapacitorSeriesT (T res_value, T cap_value, T fs = (T) 48000.0)
+            : R_value (res_value),
+              C_value (cap_value),
+              tt ((T) 1 / fs)
+        {
+            calcImpedance();
+        }
+
+        /** Prepares the capacitor to operate at a new sample rate */
+        void prepare (T sampleRate)
+        {
+            tt = (T) 1 / sampleRate;
+            propagateImpedanceChange();
+
+            reset();
+        }
+
+        /** Resets the capacitor state */
+        void reset()
+        {
+            z = (T) 0.0;
+        }
+
+        /** Sets the resistance value of the WDF resistor, in Ohms. */
+        void setResistanceValue (T newR)
+        {
+            if (all (newR == R_value))
+                return;
+
+            R_value = newR;
+            propagateImpedanceChange();
+        }
+
+        /** Sets the capacitance value of the WDF capacitor, in Farads. */
+        void setCapacitanceValue (T newC)
+        {
+            if (all (newC == C_value))
+                return;
+
+            C_value = newC;
+            propagateImpedanceChange();
+        }
+
+        /** Computes the impedance of the WDF resistor/capacitor combination */
+        inline void calcImpedance() override
+        {
+            wdf.R = tt / ((T) 2.0 * C_value) + R_value;
+            wdf.G = (T) 1.0 / wdf.R;
+            T_over_T_plus_2RC = tt / ((T) 2 * C_value * R_value + tt);
+        }
+
+        /** Accepts an incident wave into the WDF. */
+        inline void incident (T x) noexcept
+        {
+            wdf.a = x;
+            z = wdf.a;
+        }
+
+        /** Propogates a reflected wave from the WDF. */
+        inline T reflected() noexcept
+        {
+            wdf.b += T_over_T_plus_2RC * (z - wdf.b);
+            return wdf.b;
+        }
+
+        WDFMembers<T> wdf;
+
+    private:
+        T R_value = (T) 1.0e3;
+        T C_value = (T) 1.0e-6;
+
+        T T_over_T_plus_2RC = (T) 0.0;
+
+        T z = (T) 0.0;
+
+        T tt;
+    };
+
+    /** WDF Resistor and Capacitor in parallel */
+    template <typename T>
+    class ResistorCapacitorParallelT final : public BaseWDF
+    {
+    public:
+        /** Creates a new WDF Resistor/Capacitor Parallel.
+         * @param res_value: Resistance value in Ohms
+         * @param cap_value: Capacitance value in Farads
+         * @param fs: WDF sample rate
+         */
+        explicit ResistorCapacitorParallelT (T res_value, T cap_value, T fs = (T) 48000.0)
+            : R_value (res_value),
+              C_value (cap_value),
+              tt ((T) 1 / fs)
+        {
+            calcImpedance();
+        }
+
+        /** Prepares the capacitor to operate at a new sample rate */
+        void prepare (T sampleRate)
+        {
+            tt = (T) 1 / sampleRate;
+            propagateImpedanceChange();
+
+            reset();
+        }
+
+        /** Resets the capacitor state */
+        void reset()
+        {
+            z = (T) 0.0;
+        }
+
+        /** Sets the resistance value of the WDF resistor, in Ohms. */
+        void setResistanceValue (T newR)
+        {
+            if (all (newR == R_value))
+                return;
+
+            R_value = newR;
+            propagateImpedanceChange();
+        }
+
+        /** Sets the capacitance value of the WDF capacitor, in Farads. */
+        void setCapacitanceValue (T newC)
+        {
+            if (all (newC == C_value))
+                return;
+
+            C_value = newC;
+            propagateImpedanceChange();
+        }
+
+        /** Computes the impedance of the WDF resistor/capacitor combination */
+        inline void calcImpedance() override
+        {
+            const auto twoRC = (T) 2.0 * C_value * R_value;
+            wdf.R = R_value * tt / (twoRC + tt);
+            wdf.G = (T) 1.0 / wdf.R;
+            twoRC_over_twoRC_plus_T = twoRC / (twoRC + tt);
+        }
+
+        /** Accepts an incident wave into the WDF. */
+        inline void incident (T x) noexcept
+        {
+            wdf.a = x;
+            z = wdf.a;
+        }
+
+        /** Propogates a reflected wave from the WDF. */
+        inline T reflected() noexcept
+        {
+            wdf.b = twoRC_over_twoRC_plus_T * (wdf.b + z) - wdf.b;
+            return wdf.b;
+        }
+
+        WDFMembers<T> wdf;
+
+    private:
+        T R_value = (T) 1.0e3;
+        T C_value = (T) 1.0e-6;
+
+        T twoRC_over_twoRC_plus_T = (T) 0.0;
+
+        T z = (T) 0.0;
+
+        T tt;
+    };
 } // namespace wdft
 } // namespace chowdsp
 
