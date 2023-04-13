d60045a7720c6d628b03ae946fd1ebd8e244d31c
jatin <jatinchowdhury18@gmail.com>
2023-04-13 02:32:47 -0700

    Add Resistor/Capacitor combo elements

---
+++ tests/CombinedComponentTest.cpp
@@ -0,0 +1,55 @@
+#include <catch2/catch2.hpp>
+#include <chowdsp_wdf/chowdsp_wdf.h>
+
+using namespace chowdsp::wdft;
+
+TEST_CASE ("Combined Component Test")
+{
+    SECTION ("Resistor/Capacitor Series")
+    {
+        static constexpr auto r_val = 2000.0f;
+        static constexpr auto c_val = 2.0e-6f;
+
+        ResistorT<float> r1 { r_val };
+        CapacitorT<float> c1 { c_val };
+        WDFSeriesT<float, decltype (r1), decltype (c1)> s1 { r1, c1 };
+
+        ResistorCapacitorSeriesT<float> rc1 { r_val, c_val };
+
+        float inputs[] = { 0.0f, 1.0f, -1.0f, 2.0f, -3.0f };
+        for (auto& a : inputs)
+        {
+            s1.incident (a);
+            const auto ref = s1.reflected();
+
+            rc1.incident (a);
+            const auto actual = rc1.reflected();
+
+            REQUIRE (ref == Approx { actual }.margin (1.0e-4f));
+        }
+    }
+
+    SECTION ("Resistor/Capacitor Parallel")
+    {
+        static constexpr auto r_val = 2000.0f;
+        static constexpr auto c_val = 2.0e-6f;
+
+        ResistorT<float> r1 { r_val };
+        CapacitorT<float> c1 { c_val };
+        WDFParallelT<float, decltype (r1), decltype (c1)> p1 { r1, c1 };
+
+        ResistorCapacitorParallelT<float> rc1 { r_val, c_val };
+
+        float inputs[] = { 0.0f, 1.0f, -1.0f, 2.0f, -3.0f };
+        for (auto& a : inputs)
+        {
+            p1.incident (a);
+            const auto ref = p1.reflected();
+
+            rc1.incident (a);
+            const auto actual = rc1.reflected();
+
+            REQUIRE (ref == Approx { actual }.margin (1.0e-4f));
+        }
+    }
+}
