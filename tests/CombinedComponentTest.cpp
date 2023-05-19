aa56a0caadcc0b34c6ec763409a899a44a1db83e
jatin <jatinchowdhury18@gmail.com>
2023-05-18 23:03:15 -0700

    Add ResistiveCapacitiveVoltageSource

--- tests/CombinedComponentTest.cpp
+++ tests/CombinedComponentTest.cpp
@@ -52,4 +52,32 @@ TEST_CASE ("Combined Component Test")
             REQUIRE (ref == Approx { actual }.margin (1.0e-4f));
         }
     }
+
+    SECTION ("Resistor/Capacitor/Voltage Source Series")
+    {
+        static constexpr auto r_val = 2000.0f;
+        static constexpr auto c_val = 2.0e-6f;
+        static constexpr auto source_v = 1.5f;
+
+        ResistiveVoltageSourceT<float> rv1 { r_val };
+        rv1.setVoltage (source_v);
+        CapacitorT<float> c1 { c_val };
+        WDFSeriesT<float, decltype (rv1), decltype (c1)> s1 { rv1, c1 };
+
+        ResistiveCapacitiveVoltageSourceT<float> rc1 { r_val, c_val };
+        rc1.setVoltage (source_v);
+        rc1.reset();
+
+        float inputs[] = { 0.0f, 1.0f, -1.0f, 2.0f, -3.0f };
+        for (auto& a : inputs)
+        {
+            s1.incident (a);
+            const auto ref = s1.reflected();
+
+            rc1.incident (a);
+            const auto actual = rc1.reflected();
+
+            REQUIRE (ref == Approx { actual }.margin (1.0e-4f));
+        }
+    }
 }
